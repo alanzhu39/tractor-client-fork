96c7da1a1a7453bbd7c3c7e2e4af02f4109ed7ae
adamczheng <53493186+adamczheng@users.noreply.github.com>
2019-07-30 12:40:30 -0700

    Add files via upload

--- single player/deck.py
+++ single player/deck.py
@@ -1,1 +1,14 @@
-
+class Deck(object):
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    suits = ['\u2663', '\u2666', '\u2665', '\u2660']
+    def __init__(self):
+        self.ids = []
+        self.card_names = []
+        for i in range(108):
+            self.ids.append(i)
+        for r in ranks:
+            for s in suits:
+                self.card_names.append(ranks[i]+suits[i])
+deck = Deck()
+for s in deck.card_names:
+    print(s)
