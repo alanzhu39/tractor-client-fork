10be1ea7f65c98b3761a7c8cccecafc4b259a549
adamczheng <53493186+adamczheng@users.noreply.github.com>
2019-07-30 14:26:17 -0700

    Create round.py

---
+++ single player/round.py
@@ -0,0 +1,51 @@
+'''
+Keep track of:
+everyone's cards
+current Zhuang Jia
+Zhuang Jia discard
+current trump
+current attacker's points
+
+also play the round:
+dealing, compare cards, and playing
+'''
+from deck import *
+class Round(object):
+    '''
+    player = list of Player (size = 4)
+    ''''
+    num_di_pai = 8
+    def __init__(self, players):
+        assert(len(players) == 4)
+        self.players = players
+        
+        set_zhuang_jia = False
+        for i in range(len(players)):
+            if players[i].zhuang_jia:
+                self.zhuang_jia_id = i
+                set_zhuang_jia = True
+        assert set_zhuang_jia
+
+    def deal(self):
+        self.deck = Deck()
+        deck.shuffle()
+        self.current_drawer = zhuang_jia_id
+        while len(deck) > num_di_pai:
+            players[current_drawer].draw()
+            self.liang_query()
+
+    def liang_query(self):
+        # format is "suit cnt" or "SJo 2" or "BJo 2"
+        print("Liang?")
+        response = input()
+        
+
+    # compare two cards
+    def cmp_cards(a, b):
+
+    def play_turn(self):
+        
+
+    def play_round(self):
+        Round.deal()
+        
