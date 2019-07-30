559ee70fbdcf5b85249f3c1fbb1836bf80a4ccb6
adamczheng <53493186+adamczheng@users.noreply.github.com>
2019-07-30 14:41:59 -0700

    Update round.py

--- single player/round.py
+++ single player/round.py
@@ -26,10 +26,13 @@ class Round(object):
                 set_zhuang_jia = True
         assert set_zhuang_jia
 
+        self.trump_suit = "none"
+        self.trump_suit_cnt = 0
+
     def deal(self):
         self.deck = Deck()
-        deck.shuffle()
-        self.current_drawer = zhuang_jia_id
+        self.deck.shuffle()
+        self.current_drawer = self.zhuang_jia_id
         while len(deck) > num_di_pai:
             players[current_drawer].draw()
             self.liang_query()
@@ -43,16 +46,22 @@ class Round(object):
         if response[1] != "1" and response[1] != "2":
             return
         if (response[0] == "SJo" or response[0] == "BJo") and response[1] == "2":
-            self.trump_suit = "wu zhu"
-            self.trump_suit_cnt = 2
-        
+            new_trump_suit = "wu zhu"
+            new_trump_suit_cnt = 3
+        if response[0] in Card.suit_map:
+            new_trump_suit = response[0]
+            new_trump_suit_cnt = int(response[1])
+        if new_trump_suit_cnt > trump_suit_cnt:
+            print("Set trump suit to: " + new_trump_suit)
+            self.trump_suit = new_trump_suit
+            self.trump_suit_cnt = new_trump_suit_cnt
         
 
     # compare two cards
     def cmp_cards(a, b):
-
+        1
     def play_turn(self):
-        
+        1
 
     def play_round(self):
         Round.deal()
