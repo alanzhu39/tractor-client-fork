dd58c263386d411db0b6f58fc1f46a31b785d252
adamczheng <53493186+adamczheng@users.noreply.github.com>
2019-07-31 15:45:20 -0700

    clean up

--- single_player/player.py
+++ single_player/player.py
@@ -1,18 +1,28 @@
-'''
+"""
 Keep track of:
 player's hand
-'''
-import operator
-class Player():
-    def __init__(self, name, trump_suit = 'spades', zhuang_jia = False, attacker = False, hand = []):
+"""
+
+
+class Player(object):
+    """
+    name = Player's name (string)
+    trump_rank = trump rank (string)
+    trump_suit = trump suit (string)
+    is_zhuang_jia = (True if player is zhuang jia, False otherwise) (boolean)
+    is_attacker = (True if player is an attacker, False otherwise) (boolean)
+    hand = list of cards in player's hand (list of Card objects)
+    """
+    def __init__(self, name, trump_rank, trump_suit='spades', is_zhuang_jia=False, is_attacker=False, hand=[]):
         self.name = name
-        self.zhuang_jia = zhuang_jia
+        self.is_zhuang_jia = is_zhuang_jia
         self.hand = hand[:]
         self.trump_suit = trump_suit
-        self.attacker = attacker
+        self.is_attacker = is_attacker
+        self.trump_rank = trump_rank
 
     def __str__(self):
-        return self.name + " " + str(self.zhuang_jia) + " " + str(self.hand)
+        return self.name + " " + str(self.is_zhuang_jia) + " " + str(self.hand)
 
     def get_name(self):
         return self.name
@@ -23,14 +33,8 @@ class Player():
     def get_trump_rank(self):
         return self.trump_rank
 
-    def get_zhuang_jia(self):
-        return self.zhuang_jia
-
     def get_hand(self):
         return self.hand
-
-    def get_attacker(self):
-        return self.attacker
 
     def set_name(self, name):
         self.name = name
@@ -41,87 +45,44 @@ class Player():
     def set_trump_rank(self, trump_rank):
         self.trump_rank = trump_rank
 
-    def set_zhuang_jia(self, zhuang_jia):
-        self.zhuang_jia = zhuang_jia
+    def set_is_zhuang_jia(self, is_zhuang_jia):
+        self.is_zhuang_jia = is_zhuang_jia
 
     def set_hand(self, hand):
         self.hand = hand
 
-    def set_attacker(self, attacker):
-        self.attacker = attacker
+    def set_is_attacker(self, is_attacker):
+        self.is_attacker = is_attacker
 
     def get_hand_size(self):
         return len(self.hand)
 
     def print_hand(self):
         for card in self.hand:
-            print (card,end=' ')
+            print(card, end=' ')
         print('')
 
     def draw(self, card):
-        #draws a card and checks to see if the player wants to declare trump suit
+        # draws a card and checks to see if the player wants to declare trump suit
         inserted = False
         for i in range(len(self.hand)):
-            if self.cmp(card, self.hand[i], self.trump_suit, self.trump_rank) >= 0:
-                self.hand.insert(i,card)
+            if card_cmp(card, self.hand[i], self.trump_suit, self.trump_rank) >= 0:
+                self.hand.insert(i, card)
                 inserted = True
                 break
         if not inserted:
             self.hand.append(card)
 
-    def play(self, card):
-        if card in self.hand:
-            self.hand.remove(card)
-            return card
-        else:
-            return "Card not in hand"
-
-    def sort(self, trump_rank, trump_suit):
+    def sort(self):
         new_hand = []
         for card in self.hand:
             for i in range(len(new_hand)):
-                if self.cmp(card, new_hand[i], self.trump_suit, self.trump_rank) >= 0:
+                if card_cmp(card, new_hand[i], self.trump_suit, self.trump_rank) >= 0:
                     new_hand.insert(i, card)
                     break
         self.hand = new_hand[:]
 
-    def cmp(self, card1, card2, trump_suit, trump_rank):
-        if card1 == card2:
-            return 0
-        if card1.is_big_joker:
-            return 1
-        if card2.is_big_joker:
-            return -1
-        if card1.is_small_joker:
-            return 1
-        if card2.is_small_joker:
-            return -1
-        if card1.rank == trump_rank and card1.suit == trump_suit:
-            return 1
-        if card2.rank == trump_rank and card2.suit == trump_suit:
-            return -1
-        if card1.rank == trump_rank and card2.rank == trump_rank:
-            return 0
-        if card1.rank == trump_rank:
-            return 1
-        if card2.rank == trump_rank:
-            return -1
-
-        suit_dict = {'clubs': 1, 'diamonds': 2, 'hearts': 3, 'spades': 4}
-        suit_dict[trump_suit] = 5
-        rank_dict = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
-        rank_dict[trump_rank] = 15
-        if suit_dict[card1.suit] > suit_dict[card2.suit]:
-            return 1
-        elif suit_dict[card1.suit] < suit_dict[card2.suit]:
-            return -1
-        else:
-            if rank_dict[card1.rank] > rank_dict[card2.rank]:
-                return 1
-            else:
-                return -1
-
-    #COUNTS THE NUMBER OF CARDS A USER HAS OF A CERTAIN KIND (determined by string input)
+    # number of occurrences of the given card in hand
     def card_count(self, card):
         res = 0
         for c in self.hand:
@@ -130,4 +91,39 @@ class Player():
         return res
 
 
+# compares cards correctly if the two cards are not both non-trump (-1 means <, 0 means =, 1 means >)
+def card_cmp(card1, card2, trump_suit, trump_rank):
+    if card1 == card2:
+        return 0
+    if card1.is_big_joker:
+        return 1
+    if card2.is_big_joker:
+        return -1
+    if card1.is_small_joker:
+        return 1
+    if card2.is_small_joker:
+        return -1
+    if card1.rank == trump_rank and card1.suit == trump_suit:
+        return 1
+    if card2.rank == trump_rank and card2.suit == trump_suit:
+        return -1
+    if card1.rank == trump_rank and card2.rank == trump_rank:
+        return 0
+    if card1.rank == trump_rank:
+        return 1
+    if card2.rank == trump_rank:
+        return -1
+
+    suit_dict = {'clubs': 1, 'diamonds': 2, 'hearts': 3, 'spades': 4, trump_suit: 5}
+    rank_dict = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12,
+                 'K': 13, 'A': 14, trump_rank: 15}
+    if suit_dict[card1.suit] > suit_dict[card2.suit]:
+        return 1
+    elif suit_dict[card1.suit] < suit_dict[card2.suit]:
+        return -1
+    else:
+        if rank_dict[card1.rank] > rank_dict[card2.rank]:
+            return 1
+        else:
+            return -1
 
--- single_player/round.py
+++ single_player/round.py
@@ -1,4 +1,4 @@
-'''
+"""
 Keep track of:
 everyone's cards
 current Zhuang Jia
@@ -8,30 +8,32 @@ current attacker's points
 
 also play the round:
 dealing, compare cards, and playing
-'''
+"""
 from single_player.deck import *
 import single_player.player_input_methods as pim
-from single_player.deck import Deck
 
 
 class Round(object):
-    '''
-    player = list of Player (size = 4)
-    '''
+    """
+    players = list of Player (size = 4) (list of Player object)
+    zhuang_jia_id = ID of zhuang jia in players (int)
+    set_zhuang_jia = True if there is a zhuang jia at the start of the round (boolean)
+    trump_suit = trump suit (string)
+    trump_suit_cnt = number of cards used to liang the trump suit (exceptions: 0 if no liang and 3 if wu zhu)
+    """
     num_di_pai = 8
+
     def __init__(self, players):
         self.deck = Deck()
         assert(len(players) == 4)
         self.players = players
-        
-        set_zhuang_jia = False
+
+        # find ID of zhuang jia, set_zhuang_jia is True if someone is zhuang jia
+        self.set_zhuang_jia = False
         for i in range(len(players)):
-            if players[i].zhuang_jia:
+            if players[i].is_zhuang_jia:
                 self.zhuang_jia_id = i
-                set_zhuang_jia = True
-        # draw for zhuang jia
-        if not set_zhuang_jia:
-            1
+                self.set_zhuang_jia = True
 
         self.trump_suit = "none"
         self.trump_suit_cnt = 0
@@ -45,18 +47,21 @@ class Round(object):
             self.players[current_drawer].print_hand()
             self.liang_query(current_drawer)
             current_drawer = (current_drawer + 1) % 4
+        # no liang -> flip di pai
         if self.trump_suit == "none":
             self.flip_di_pai()
+        # zhuang jia chooses 8 cards for di pai
         self.choose_di_pai()
-
 
     def liang_query(self, current_drawer):
         # format is "suit cnt" or "SJo 2" or "BJo 2"
         print("Liang?")
         response = input().split()
+        # "n" "no" or nothing means no liang
         if len(response) == 0 or response[0] == "n" or response[0] == "no":
             print("No liang, continuing")
             return
+        # check for validity of the response
         if len(response) != 2:
             print("invalid response, continuing")
             return
@@ -64,14 +69,18 @@ class Round(object):
             print("invalid response, continuing")
             return
         if (response[0] == "SJo" or response[0] == "BJo") and response[1] == "2":
+            # wu zhu liang
             new_trump_suit = "wu zhu"
             new_trump_suit_cnt = 3
         elif response[0] in Card.suit_map:
+            # other liang
             new_trump_suit = response[0]
             new_trump_suit_cnt = int(response[1])
         else:
             print("invalid response, continuing")
             return
+
+        # check whether the liang is valid
         if response[0] == "SJo":
             card_to_check = SMALL_JOKER
             cnt_to_check = 2
@@ -81,22 +90,25 @@ class Round(object):
         else:
             card_to_check = Card(self.players[0].get_trump_rank(), new_trump_suit)
             cnt_to_check = new_trump_suit_cnt
-        if new_trump_suit_cnt > self.trump_suit_cnt:
-            if self.players[current_drawer].card_count(card_to_check) >= cnt_to_check:
+
+        if self.players[current_drawer].card_count(card_to_check) >= cnt_to_check:
+            if new_trump_suit_cnt > self.trump_suit_cnt:
                 print("Set trump suit to: " + new_trump_suit)
                 self.trump_suit = new_trump_suit
                 self.trump_suit_cnt = new_trump_suit_cnt
             else:
                 print("You don't have the cards necessary for that liang")
+        else:
+            print("You don't have the cards necessary for that liang")
         
 
-    # compare two cards
     def cmp_cards(a, b):
         1
+
     def play_turn(self):
         1
 
-    def flip_di_pair(self):
+    def flip_di_pai(self):
         1
 
     def choose_di_pai(self):
@@ -104,7 +116,8 @@ class Round(object):
 
     def play_round(self):
         self.deal()
-
+        while len(self.players[0].get_hand()) > 0:
+            self.play_turn()
 
     def turn_helper(self, start):
         hand_size=len(self.players[0].gethand())
