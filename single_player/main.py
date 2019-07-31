4b2c250fb5cd47b45d8aa02a8278813081ddd001
adamczheng <53493186+adamczheng@users.noreply.github.com>
2019-07-31 14:51:20 -0700

    updated some stuff

--- single_player/main.py
+++ single_player/main.py
@@ -3,11 +3,13 @@ add players
 start the rounds
 keep track of scores
 '''
-from player import *
-from round import *
+from single_player.player import *
+from single_player.round import *
 
 players = [Player("Adam"), Player("Andrew"), Player("Alan"), Player("Raymond")]
 players[0].set_zhuang_jia(True)
+for i in range(4):
+    players[i].set_trump_rank('2')
 
 round = Round(players)
 round.play_round()
