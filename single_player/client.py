03afedef466f3952ad2063dba883479a7e57797b
alanzhu39 <36008118+alanzhu39@users.noreply.github.com>
2019-08-23 15:29:49 -0700

    more client work

--- single_player/client.py
+++ single_player/client.py
@@ -19,14 +19,17 @@ r = Round(players)
 deck_dict = {}
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ['C', 'D', 'H', 'S']
-suit_map = {'C': 'clubs','D': 'diamonds','H': 'hearts','S': 'spades'}
+suit_map = {'C': '\u2663','D': '\u2666','H': '\u2665','S': '\u2660'}
 for rank in ranks:
     for s in suits:
         card_key = rank + suit_map[s]
         card_file = "cards_jpeg\\" + rank + s + ".jpg"
-        deck_dict[card_key] = pygame.transform.scale(pygame.image.load(card_file),(70,110))
-deck_dict['BJo'] = pygame.transform.scale(pygame.image.load("cards_jpeg\\BJo.jpg"),(70,110))
-deck_dict['SJo'] = pygame.transform.scale(pygame.image.load("cards_jpeg\\SJo.jpg"),(70,110))
+        my_card = pygame.transform.scale(pygame.image.load(card_file),(90,140))
+        my_card.set_colorkey(pygame.Color(0,0,0))
+        deck_dict[card_key] = pygame.transform.scale(pygame.image.load(card_file),(90,140))
+deck_dict['BJo'] = pygame.transform.scale(pygame.image.load("cards_jpeg\\BJo.jpg"),(90,140))
+deck_dict['SJo'] = pygame.transform.scale(pygame.image.load("cards_jpeg\\SJo.jpg"),(90,140))
+print(deck_dict)
 
 class TractorClient():
 
@@ -60,28 +63,23 @@ class TractorClient():
             player_hand_cards = player.get_hand()
             player_hand = []
             for raw_card in player_hand_cards:
-                if raw_card.get_is_big_joker:
-                    player_hand.append("BJo")
-                elif raw_card.get_is_small_joker:
-                    player_hand.append("SJo")
-                else:
-                    parsed_card = raw_card.get_rank() + raw_card.get_suit()
-                    player_hand.append(parsed_card)
+                player_hand.append(str(raw_card))
             player_hands.append(player_hand)
         user_hand = player_hands[self.playerID]
-        print(user_hand)
         right_hand = player_hands[(self.playerID + 1) % 4]
         across_hand = player_hands[(self.playerID + 2) % 4]
         left_hand = player_hands[(self.playerID + 3) % 4]
 
         # draws user's hand
-        left_coord = 450 - (18*(len(user_hand)-1)+70)/2
+        left_coord = 450 - (20*(len(user_hand)-1)+90)/2
         for card_index in range(len(user_hand)):
             if card_index < len(user_hand) - 1:
-                self.screen.blit(deck_dict[user_hand[card_index]], [left_coord+18*(card_index), 490], area=pygame.Rect(0, 0, 18, 110), special_flags=0)
+                self.screen.blit(deck_dict[user_hand[card_index]], [left_coord+20*(card_index), 460], area=pygame.Rect(0, 0, 20, 140), special_flags=0)
             else:
-                self.screen.blit(deck_dict[user_hand[card_index]], [left_coord+18*(card_index), 490], area=None, special_flags=0)
+                self.screen.blit(deck_dict[user_hand[card_index]], [left_coord+20*(card_index), 460], area=None, special_flags=0)
             card_index += 1
+
+
 
     def update(self):
         # make the game 60 fps
