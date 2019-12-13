97e80e26c50bed2552a6877b1ead103d42a70795
alanzhu39 <36008118+alanzhu39@users.noreply.github.com>
2019-12-13 11:49:24 -0800

    server port change

--- single_player/network.py
+++ single_player/network.py
@@ -8,7 +8,7 @@ class Network:
         self.host = "136.152.143.34"  # For this to work on your machine this must be equal to the ipv4 address of the
         # machine running the server You can find this address by typing ipconfig in CMD and copying the ipv4
         # address. Again this must be the servers ipv4 address. This field will be the same for all your clients.
-        self.port = 5554
+        self.port = 5555
         self.addr = (self.host, self.port)
         self.id = self.connect()
 
